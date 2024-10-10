6c08fa408adb26b2f6655367bcff16a20b795c6d
Indy Iri <indy.iri@student.pxl.be>
2024-10-10 23:19:26 +0200

    Eigen extra's toegevoegd

--- H3Oef2Cursusgeld/H3Oef2Cursusgeld/MainWindow.xaml.cs
+++ H3Oef2Cursusgeld/H3Oef2Cursusgeld/MainWindow.xaml.cs
@@ -37,9 +37,37 @@ namespace H3Oef2Cursusgeld
             {
                 numericalTextBlock.Text = "Is numeriek";
             }
+            else if (string.IsNullOrEmpty(yearText))
+            {
+                numericalTextBlock.Text = "Geef een jaartal in!";
+            }
             else
             {
                 numericalTextBlock.Text = "Geef een correct jaartal!";
+            }
+        }
+
+        private void yearTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                string yearText = yearTextBox.Text;
+                int yearNumber;
+
+                bool isYearTextNumber = int.TryParse(yearText, out yearNumber);
+
+                if (isYearTextNumber)
+                {
+                    numericalTextBlock.Text = "Is numeriek";
+                }
+                else if (string.IsNullOrEmpty(yearText))
+                {
+                    numericalTextBlock.Text = "Geef een jaartal in!";
+                }
+                else
+                {
+                    numericalTextBlock.Text = "Geef een correct jaartal!";
+                }
             }
         }
 
@@ -53,7 +81,7 @@ namespace H3Oef2Cursusgeld
             bool isYearTextNumber = int.TryParse(yearText, out yearNumber);
             bool isAmountOfHoursNumber = int.TryParse(amountOfHoursText, out amountOfHoursNumber);
 
-            if (isYearTextNumber && yearNumber % 4 == 0 && yearNumber % 100 != 0)
+            if (isYearTextNumber && yearNumber % 4 == 0 && yearNumber % 100 != 0 )
             {
                 leapYearTextBlock.Text = "Is een schrikkeljaar";
                 registrationFeeTextBox.Text = $"{(amountOfHoursNumber + 1)*1.5}";
@@ -62,6 +90,36 @@ namespace H3Oef2Cursusgeld
             {
                 leapYearTextBlock.Text = "Is geen schrikkeljaar";
                 registrationFeeTextBox.Text = $"{(amountOfHoursNumber) * 1.5}";
+            }
+            else if (string.IsNullOrWhiteSpace(yearText) || string.IsNullOrWhiteSpace(amountOfHoursText))
+            {
+                leapYearTextBlock.Text = "Geef een jaartal en aantal uren in";
+            }
+        }
+
+        private void amountOfHoursTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            string yearText = yearTextBox.Text;
+            int yearNumber;
+            string amountOfHoursText = amountOfHoursTextBox.Text;
+            int amountOfHoursNumber;
+
+            bool isYearTextNumber = int.TryParse(yearText, out yearNumber);
+            bool isAmountOfHoursNumber = int.TryParse(amountOfHoursText, out amountOfHoursNumber);
+
+            if (isYearTextNumber && yearNumber % 4 == 0 && yearNumber % 100 != 0)
+            {
+                leapYearTextBlock.Text = "Is een schrikkeljaar";
+                registrationFeeTextBox.Text = $"{(amountOfHoursNumber + 1) * 1.5}";
+            }
+            else if (isYearTextNumber)
+            {
+                leapYearTextBlock.Text = "Is geen schrikkeljaar";
+                registrationFeeTextBox.Text = $"{(amountOfHoursNumber) * 1.5}";
+            }
+            else if (string.IsNullOrWhiteSpace(yearText) || string.IsNullOrWhiteSpace(amountOfHoursText))
+            {
+                leapYearTextBlock.Text = "Geef een jaartal en aantal uren in";
             }
         }
     }
